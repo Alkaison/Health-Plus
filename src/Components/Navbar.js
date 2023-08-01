2e46a9c31e873d4500951fd5cc8f4b25109063ce
Alkaison <505ganeshmourya@gmail.com>
2023-08-01 14:25:01 +0530

    Feat: Navbar component with stylesheet

---
+++ src/Components/Navbar.js
@@ -0,0 +1,44 @@
+import React from "react";
+import "../Styles/Navbar.css";
+
+function Navbar() {
+  return (
+    <div className="navbar-section">
+      <h1 className="navbar-title">
+        <a href="/">
+          Health <span className="navbar-sign">+</span>
+        </a>
+      </h1>
+      <ul className="navbar-items">
+        <li>
+          <a href="/" className="navbar-links">
+            Home
+          </a>
+        </li>
+        <li>
+          <a href="#services" className="navbar-links">
+            Services
+          </a>
+        </li>
+        <li>
+          <a href="#doctors" className="navbar-links">
+            Doctors
+          </a>
+        </li>
+        <li>
+          <a href="#reviews" className="navbar-links">
+            Reviews
+          </a>
+        </li>
+        <li>
+          <a href="#about" className="navbar-links">
+            About
+          </a>
+        </li>
+      </ul>
+      <button className="navbar-btn" type="button">Live Chat</button>
+    </div>
+  );
+}
+
+export default Navbar;
