0d7cd5cf72e611d12573d8932e1cd7d4ddd2f20a
Alkaison <505ganeshmourya@gmail.com>
2023-08-01 14:24:40 +0530

    style: global CSS code

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
+import "./App.css";
+import Navbar from "./Components/Navbar";
 
 function App() {
   return (
     <div className="App">
-      Hello World
+      <Navbar />
     </div>
   );
 }
