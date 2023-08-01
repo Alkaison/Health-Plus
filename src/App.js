8bb3014277aee6951fbd31f2c5fc2d7ee7cd5bf8
Alkaison <505ganeshmourya@gmail.com>
2023-08-01 16:24:42 +0530

    Feat: hero section component with stylesheet

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
 import Navbar from "./Components/Navbar";
+import Hero from "./Components/Hero";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Hero />
     </div>
   );
 }
---
+++ src/Components/Hero.js
@@ -0,0 +1,47 @@
+import React from "react";
+import Doctor from "../Assets/doctor-picture.png";
+import "../Styles/Hero.css";
+
+function Hero() {
+  return (
+    <div className="hero-section">
+      <div className="text-section">
+        <p className="text-headline">❤️ Health comes first</p>
+        <h2 className="text-title">
+          Find your Doctor and make an Appointments
+        </h2>
+        <p className="text-descritpion">
+          Talk to online doctors and get medical advice, online prescriptions,
+          refills and medical notes within minutes.
+          <br />
+          On-demand healthcare services at your fingertips.
+        </p>
+        <button className="text-appointment-btn" type="button">
+          Book Appointment
+        </button>
+        <div className="text-stats">
+          <div className="text-stats-container">
+            <p>145k+</p>
+            <p>Receive Patients</p>
+          </div>
+
+          <div className="text-stats-container">
+            <p>50+</p>
+            <p>Expert Doctors</p>
+          </div>
+
+          <div className="text-stats-container">
+            <p>10+</p>
+            <p>Years of Experience</p>
+          </div>
+        </div>
+      </div>
+
+      <div className="image-section">
+        <img className="image1" src={Doctor} alt="Doctor" />
+      </div>
+    </div>
+  );
+}
+
+export default Hero;
