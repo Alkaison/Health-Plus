2fa304d5da9dcebbd98f6a33b7c87dcbdb14c4dc
Alkaison <505ganeshmourya@gmail.com>
2023-08-08 22:46:49 +0530

    Feat: Mobile Navbar responsive code

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,9 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCommentDots } from "@fortawesome/free-solid-svg-icons";
+import { faCommentDots, faBars, faXmark } from "@fortawesome/free-solid-svg-icons";
 import "../Styles/Navbar.css";
 
 function Navbar() {
+  const [nav, setNav] = useState(false);
+
+  const openNav = () => {
+    setNav(!nav);
+  };
+
   return (
     <div className="navbar-section">
       <h1 className="navbar-title">
@@ -11,6 +17,8 @@ function Navbar() {
           Health <span className="navbar-sign">+</span>
         </a>
       </h1>
+
+      {/* Desktop */}
       <ul className="navbar-items">
         <li>
           <a href="/" className="navbar-links">
@@ -38,7 +46,54 @@ function Navbar() {
           </a>
         </li>
       </ul>
-      <button className="navbar-btn" type="button"><FontAwesomeIcon icon={faCommentDots} /> Live Chat</button>
+
+      <button className="navbar-btn" type="button">
+        <FontAwesomeIcon icon={faCommentDots} /> Live Chat
+      </button>
+
+      {/* Mobile */}
+      <div className={`mobile-navbar ${nav ? "open-nav" : ""}`}>
+        <div onClick={openNav} className="mobile-navbar-close">
+          <FontAwesomeIcon icon={faXmark} className="hamb-icon" />
+        </div>
+
+        <ul className="mobile-navbar-links">
+          <li>
+            <a onClick={openNav} href="/">
+              Home
+            </a>
+          </li>
+          <li>
+            <a onClick={openNav} href="#services">
+              Services
+            </a>
+          </li>
+          <li>
+            <a onClick={openNav} href="#doctors">
+              Doctors
+            </a>
+          </li>
+          <li>
+            <a onClick={openNav} href="#reviews">
+              Reviews
+            </a>
+          </li>
+          <li>
+            <a onClick={openNav} href="#about">
+              About
+            </a>
+          </li>
+        </ul>
+      </div>
+
+      {/* Hamburger Icon */}
+      <div className="mobile-nav">
+        <FontAwesomeIcon
+          icon={faBars}
+          onClick={openNav}
+          className="hamb-icon"
+        />
+      </div>
     </div>
   );
 }
