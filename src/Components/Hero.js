3a68193f4028f3f752670c25ad36ed7728669c98
Alkaison <505ganeshmourya@gmail.com>
2023-08-08 20:18:31 +0530

    Feat: scrollToUp button with logic

--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -1,45 +1,75 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Doctor from "../Assets/doctor-picture.png";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCalendarCheck } from "@fortawesome/free-solid-svg-icons";
+import { faCalendarCheck, faAngleUp } from "@fortawesome/free-solid-svg-icons";
 import "../Styles/Hero.css";
 
 function Hero() {
+  const [goUp, setGoUp] = useState(false);
+
+  const scrollToTop = () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  };
+
+  useEffect(() => {
+    const onPageScroll = () => {
+      if (window.scrollY > 600) {
+        setGoUp(true);
+      } else {
+        setGoUp(false);
+      }
+    };
+    window.addEventListener("scroll", onPageScroll);
+
+    return () => {
+      window.removeEventListener("scroll", onPageScroll);
+    };
+  }, []);
+
   return (
-    <div className="hero-section">
-      <div className="text-section">
-        <p className="text-headline">❤️ Health comes first</p>
-        <h2 className="text-title">
-          Find your Doctor and make an Appointments
-        </h2>
-        <p className="text-descritpion">
-          Talk to online doctors and get medical advice, online prescriptions,
-          refills and medical notes within minutes. On-demand healthcare
-          services at your fingertips.
-        </p>
-        <button className="text-appointment-btn" type="button">
-          <FontAwesomeIcon icon={faCalendarCheck} /> Book Appointment
-        </button>
-        <div className="text-stats">
-          <div className="text-stats-container">
-            <p>145k+</p>
-            <p>Receive Patients</p>
-          </div>
+    <div className="section-container">
+      <div className="hero-section">
+        <div className="text-section">
+          <p className="text-headline">❤️ Health comes first</p>
+          <h2 className="text-title">
+            Find your Doctor and make an Appointments
+          </h2>
+          <p className="text-descritpion">
+            Talk to online doctors and get medical advice, online prescriptions,
+            refills and medical notes within minutes. On-demand healthcare
+            services at your fingertips.
+          </p>
+          <button className="text-appointment-btn" type="button">
+            <FontAwesomeIcon icon={faCalendarCheck} /> Book Appointment
+          </button>
+          <div className="text-stats">
+            <div className="text-stats-container">
+              <p>145k+</p>
+              <p>Receive Patients</p>
+            </div>
 
-          <div className="text-stats-container">
-            <p>50+</p>
-            <p>Expert Doctors</p>
-          </div>
+            <div className="text-stats-container">
+              <p>50+</p>
+              <p>Expert Doctors</p>
+            </div>
 
-          <div className="text-stats-container">
-            <p>10+</p>
-            <p>Years of Experience</p>
+            <div className="text-stats-container">
+              <p>10+</p>
+              <p>Years of Experience</p>
+            </div>
           </div>
+        </div>
+
+        <div className="hero-image-section">
+          <img className="hero-image1" src={Doctor} alt="Doctor" />
         </div>
       </div>
 
-      <div className="hero-image-section">
-        <img className="hero-image1" src={Doctor} alt="Doctor" />
+      <div
+        onClick={scrollToTop}
+        className={`scroll-up ${goUp ? "show-scroll" : ""}`}
+      >
+        <FontAwesomeIcon icon={faAngleUp} />
       </div>
     </div>
   );
