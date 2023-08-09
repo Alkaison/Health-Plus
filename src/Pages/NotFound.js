db88b82ac92aed2f5ae607830484eaf05f4d625b
Alkaison <505ganeshmourya@gmail.com>
2023-08-09 23:13:52 +0530

    Fix: pageNotFound debugging code

--- src/Pages/NotFound.js
+++ src/Pages/NotFound.js
@@ -1,9 +1,15 @@
 import React from "react";
+import { useLocation } from "react-router-dom";
 
 function NotFound() {
+  const deployedURL = window.location.href;
+  const location = useLocation();
+
   return (
     <div>
       <h1>Page Not Found</h1>
+      <p>The requested URL {deployedURL} was not found on this server.</p>
+      <p>The requested URL {location.pathname} was not found on this server.</p>
     </div>
   );
 }
