6e08a0bb6b96b70593996e9d73ca9f45a1ed4a9f
Alkaison <505ganeshmourya@gmail.com>
2023-08-09 22:10:42 +0530

    Fix: removed unused component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Home from "./Pages/Home";
 import Legal from "./Pages/Legal";
