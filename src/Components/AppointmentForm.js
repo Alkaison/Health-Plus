78c99323c3ece9b144d41dc9c89f896a45b2ee44
Alkaison <505ganeshmourya@gmail.com>
2023-08-11 01:10:50 +0530

    Feat: appointment form component with stylesheet

---
+++ src/Components/AppointmentForm.js
@@ -0,0 +1,140 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import "../Styles/AppointmentForm.css";
+import { ToastContainer, toast } from "react-toastify";
+
+function AppointmentForm() {
+  const [patientName, setPatientName] = useState("");
+  const [patientGender, setPatientGender] = useState("default");
+  const [appointmentTime, setAppointmentTime] = useState("");
+  const [preferredMode, setPreferredMode] = useState("default");
+  const [formErrors, setFormErrors] = useState({});
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // Validate form inputs
+    const errors = {};
+    if (!patientName.trim()) {
+      errors.patientName = "Patient name is required";
+    } else if (patientName.trim().length < 8) {
+      errors.patientName = "Patient name must be at least 8 characters";
+    }
+    if (patientGender === "default") {
+      errors.patientGender = "Please select patient gender";
+    }
+    if (!appointmentTime) {
+      errors.appointmentTime = "Appointment time is required";
+    } else {
+      const selectedTime = new Date(appointmentTime).getTime();
+      const currentTime = new Date().getTime();
+      if (selectedTime <= currentTime) {
+        errors.appointmentTime = "Please select a future appointment time";
+      }
+    }
+    if (preferredMode === "default") {
+      errors.preferredMode = "Please select preferred mode";
+    }
+
+    if (Object.keys(errors).length > 0) {
+      setFormErrors(errors);
+      return;
+    }
+
+    // Reset form fields and errors after successful submission
+    setPatientName("");
+    setPatientGender("default");
+    setAppointmentTime("");
+    setPreferredMode("default");
+    setFormErrors({});
+
+    toast.success("Appointment Scheduled !", {
+      position: toast.POSITION.TOP_CENTER,
+    });
+  };
+
+  return (
+    <div className="appointment-form-section">
+      <h1 className="legal-siteTitle">
+        <Link to="/">
+          Health <span className="legal-siteSign">+</span>
+        </Link>
+      </h1>
+
+      <div className="form-container">
+        <h2 className="form-title">
+          <span>Book Appointment Online</span>
+        </h2>
+
+        <form className="form-content" onSubmit={handleSubmit}>
+          <label>
+            Patient Full Name:
+            <input
+              type="text"
+              value={patientName}
+              onChange={(e) => setPatientName(e.target.value)}
+              required
+            />
+            {formErrors.patientName && <p className="error-message">{formErrors.patientName}</p>}
+          </label>
+
+          <br />
+          <label>
+            Patient Gender:
+            <select
+              value={patientGender}
+              onChange={(e) => setPatientGender(e.target.value)}
+              required
+            >
+              <option value="default">Select</option>
+              <option value="male">Male</option>
+              <option value="female">Female</option>
+              <option value="private">I will inform Doctor only</option>
+            </select>
+            {formErrors.patientGender && <p className="error-message">{formErrors.patientGender}</p>}
+          </label>
+
+          <br />
+          <label>
+            Preferred Appointment Time:
+            <input
+              type="datetime-local"
+              value={appointmentTime}
+              onChange={(e) => setAppointmentTime(e.target.value)}
+              required
+            />
+            {formErrors.appointmentTime && <p className="error-message">{formErrors.appointmentTime}</p>}
+          </label>
+
+          <br />
+          <label>
+            Preferred Mode:
+            <select
+              value={preferredMode}
+              onChange={(e) => setPreferredMode(e.target.value)}
+              required
+            >
+              <option value="default">Select</option>
+              <option value="voice">Voice Call</option>
+              <option value="video">Video Call</option>
+            </select>
+            {formErrors.preferredMode && <p className="error-message">{formErrors.preferredMode}</p>}
+          </label>
+
+          <br />
+          <button type="submit" className="text-appointment-btn">
+            Confirm Appointment
+          </button>
+        </form>
+      </div>
+
+      <div className="legal-footer">
+        <p>© 2013-2023 Health+. All rights reserved.</p>
+      </div>
+
+      <ToastContainer autoClose={4000} limit={1} closeButton={false} />
+    </div>
+  );
+}
+
+export default AppointmentForm;
---
+++ src/Pages/Appointment.js
@@ -0,0 +1,8 @@
+import React from "react";
+import AppointmentForm from "../Components/AppointmentForm";
+
+function Appointment() {
+  return <AppointmentForm />;
+}
+
+export default Appointment;
