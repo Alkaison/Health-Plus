9d52dc39fb956652214bba34bacf54aa13274911
Alkaison <505ganeshmourya@gmail.com>
2023-08-10 23:34:14 +0530

    Fix: scrollToTop when arrived on legal page

--- src/Components/LegalDocs.js
+++ src/Components/LegalDocs.js
@@ -1,8 +1,12 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
 import "../Styles/LegalDocs.css";
 
 function LegalDocs() {
+  useEffect(() => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  });
+
   return (
     <div className="legal-section-title">
       <h1 className="legal-siteTitle">
